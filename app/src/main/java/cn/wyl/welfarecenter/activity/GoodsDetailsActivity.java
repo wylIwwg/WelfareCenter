7486cba0dcb46f04e872ed93a77d3184ba468905
wylIwwg <1002800817@qq.com>
2016-10-17 20:16:59 +0800

    创建其余四个布局文件及相关Fragment

--- app/src/main/java/cn/wyl/welfarecenter/activity/GoodsDetailsActivity.java
+++ app/src/main/java/cn/wyl/welfarecenter/activity/GoodsDetailsActivity.java
@@ -1,9 +1,73 @@
 package cn.wyl.welfarecenter.activity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.wyl.welfarecenter.R;
+import cn.wyl.welfarecenter.bean.GoodsDetailsBean;
+import cn.wyl.welfarecenter.net.NetDao;
+import cn.wyl.welfarecenter.utils.ImageLoader;
+import cn.wyl.welfarecenter.utils.OkHttpUtils;
+
 /**
  * 项目名称：WelfareCenter
  * 创建人：wyl
  * 时间：2016/10/17 19:01
  */
-public class GoodsDetailsActivity {
+public class GoodsDetailsActivity extends AppCompatActivity {
+    @BindView(R.id.img_back)
+    ImageView mImgBack;
+    @BindView(R.id.img_collect)
+    ImageView mImgCollect;
+    @BindView(R.id.img_share)
+    ImageView mImgShare;
+    @BindView(R.id.tv_goodsEngName)
+    TextView mTvGoodsEngName;
+    @BindView(R.id.tv_goodsName)
+    TextView mTvGoodsName;
+    @BindView(R.id.tv_goodsPrice)
+    TextView mTvGoodsPrice;
+    @BindView(R.id.img_goods)
+    ImageView mImgGoods;
+    @BindView(R.id.tv_desc)
+    TextView mTvDesc;
+    Context mContext;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.goodsdetails);
+        ButterKnife.bind(this);
+        Intent intent = getIntent();
+        int goodid = intent.getIntExtra("id", 0);
+        initView(goodid);
+        mContext=this;
+    }
+
+    private void initView(int goodid) {
+        NetDao.downGoodsDetails(this, goodid, new OkHttpUtils.OnCompleteListener<GoodsDetailsBean>() {
+            @Override
+            public void onSuccess(GoodsDetailsBean result) {
+                mTvDesc.setText(result.getGoodsBrief());
+                mTvGoodsEngName.setText(result.getGoodsEnglishName());
+                mTvGoodsName.setText(result.getGoodsName());
+                mTvGoodsPrice.setText(result.getCurrencyPrice());
+                ImageLoader.downloadImg(mContext, mImgGoods, result.getGoodsThumb());
+
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+
+    }
 }
--- app/src/main/java/cn/wyl/welfarecenter/activity/MainActivity.java
+++ app/src/main/java/cn/wyl/welfarecenter/activity/MainActivity.java
@@ -1,17 +1,25 @@
 package cn.wyl.welfarecenter.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import cn.wyl.welfarecenter.NewGoodsFragment;
 import cn.wyl.welfarecenter.R;
+import cn.wyl.welfarecenter.adapters.NewGoodsAdapter;
+import cn.wyl.welfarecenter.bean.NewGoodsBean;
+import cn.wyl.welfarecenter.fragments.BoutiqueFragment;
+import cn.wyl.welfarecenter.fragments.CartFragment;
+import cn.wyl.welfarecenter.fragments.CategoryFragment;
+import cn.wyl.welfarecenter.fragments.NewGoodsFragment;
+import cn.wyl.welfarecenter.fragments.PersonFragment;
 
 /**
  * 添加闪屏
@@ -29,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
     TextView mTvCartHint;
     @BindView(R.id.rb_center)
     RadioButton mRbCenter;
+    RadioButton[] groupButton = new RadioButton[5];
+    Fragment[] mFragments = new Fragment[5];
+
+    NewGoodsAdapter mNewGoodsAdapter;
+    ArrayList<NewGoodsBean> mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,60 +49,79 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         initView();
-        setListeners();
 
 
-    }
-
-    private void setListeners() {
-        downLoadData();
-    }
-
-    private void downLoadData() {
     }
 
     private void initView() {
+        mList = new ArrayList<>();
+        mNewGoodsAdapter = new NewGoodsAdapter(this, mList);
         groupButton[0] = mRbNewgoods;
         groupButton[1] = mRbBoutique;
         groupButton[2] = mRbCategory;
         groupButton[3] = mRbCart;
         groupButton[4] = mRbCenter;
-
-
     }
 
 
-    RadioButton[] groupButton = new RadioButton[5];
     int index;
 
     public void onMenuButtonChanged(View view) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        hideFragments(transaction);
         switch (view.getId()) {
             case R.id.rb_newgoods:
                 index = 0;
-                NewGoodsFragment newGoodsFragment = new NewGoodsFragment();
-                transaction.add(R.id.frameLayout, newGoodsFragment).commit();
-                Log.d("main", "newgoods");
+                if (mFragments[0] == null) {
+                    mFragments[0] = new NewGoodsFragment();
+                    transaction.add(R.id.frameLayout, mFragments[0]);
+                } else
+                    transaction.show(mFragments[0]);
                 break;
             case R.id.rb_boutique:
                 index = 1;
-                Log.d("main", "boutique");
+                if (mFragments[1] == null) {
+                    mFragments[1] = new BoutiqueFragment();
+                    transaction.add(R.id.frameLayout, mFragments[1]);
+                } else
+                    transaction.show(mFragments[1]);
                 break;
             case R.id.rb_category:
                 index = 2;
-                Log.d("main", "category");
+                if (mFragments[2] == null) {
+                    mFragments[2] = new CategoryFragment();
+                    transaction.add(R.id.frameLayout, mFragments[2]);
+                } else transaction.show(mFragments[2]);
                 break;
 
             case R.id.rb_center:
                 index = 4;
-                Log.d("main", "center");
+                if (mFragments[4] == null) {
+                    mFragments[4] = new PersonFragment();
+                    transaction.add(R.id.frameLayout, mFragments[4]);
+                } else
+                    transaction.show(mFragments[4]);
                 break;
             case R.id.rb_cart:
                 index = 3;
-                Log.d("main", "cart");
+                if (mFragments[3] == null) {
+                    mFragments[3] = new CartFragment();
+                    transaction.add(R.id.frameLayout, mFragments[3]);
+                } else
+                    transaction.show(mFragments[3]);
                 break;
         }
         setRadioButtonStatus();
+        transaction.commit();
+    }
+
+    private void hideFragments(FragmentTransaction transaction) {
+        for (int i = 0; i < mFragments.length; i++) {
+            if (mFragments[i] != null) {
+                transaction.hide(mFragments[i]);
+            }
+        }
+
     }
 
     private void setRadioButtonStatus() {
