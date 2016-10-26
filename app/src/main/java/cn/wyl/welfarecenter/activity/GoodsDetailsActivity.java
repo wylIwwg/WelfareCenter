e7d556f994571d5d1abceb21f054e029d2398613
wylIwwg <1002800817@qq.com>
2016-10-26 18:20:00 +0800

    使用ShareSDK完成商品页面分享功能

--- app/src/main/java/cn/wyl/welfarecenter/activity/GoodsDetailsActivity.java
+++ app/src/main/java/cn/wyl/welfarecenter/activity/GoodsDetailsActivity.java
@@ -5,13 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
 import android.util.Log;
-import android.view.Display;
-import android.view.Gravity;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +13,8 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.wyl.welfarecenter.I;
 import cn.wyl.welfarecenter.R;
 import cn.wyl.welfarecenter.WelfareCenterApplication;
@@ -107,6 +103,38 @@ public class GoodsDetailsActivity extends BaseActivity {
     }
 
 
+    @OnClick(R.id.img_share)
+    public void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(this);
+    }
+
     @OnClick(R.id.img_collect)
     public void collectGoods() {
         if (user == null) {
@@ -184,7 +212,7 @@ public class GoodsDetailsActivity extends BaseActivity {
         MFGT.finish(this);
     }
 
-    @OnClick(R.id.img_share)
+   /* @OnClick(R.id.img_share)
     public void onShareGoods() {
         View share = getLayoutInflater().inflate(R.layout.activity_share, null);
         AlertDialog.Builder dialogShare = new AlertDialog.Builder(this).
@@ -197,11 +225,11 @@ public class GoodsDetailsActivity extends BaseActivity {
        /* WindowManager.LayoutParams params = window.getAttributes();
         params.width = display.getWidth();
         params.alpha = 0.7f;
-        window.setAttributes(params);*/
+        window.setAttributes(params);
         window.setGravity(Gravity.BOTTOM);
         dialogShare.show();
 
-    }
+    }*/
 
     private int getAlbumCount(GoodsDetailsBean result) {
         if (result.getProperties() != null && result.getProperties().length > 0) {
